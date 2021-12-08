440f46a9ff445ef6c42724432d9547bfcc2f7df9
Pacote022 <71948252+Pacote022@users.noreply.github.com>
2021-12-08 15:46:58 +0000

    'teste'

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var app = express ();
-app.use ('/', (req , res ) => {
-    res.send ('Hello World !');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) =>{
+	res.send('Hello world!!!');
 });
-app.listen (3000 , () => {
-    console.log('listen on port 3000');
-}
-);
+
+app.get('/api/courses', (req, res) => {
+	res.send([1,2,3]);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
